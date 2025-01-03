ab6fdc0c8e2db1a7a5225b0c4486f993c6371291
Mohamed El farssi <you@example.com>
2025-01-03 17:22:42 +0100

    some models

--- app/http/controllers/homeController.go
+++ app/http/controllers/homeController.go
@@ -1,11 +1,18 @@
 package controllers
 
 import (
-	"forum/config"
 	"net/http"
 )
-func HomeController(w http.ResponseWriter, r *http.Request) {
-	config.DatabaseExecution()
-	
 
+func HomeController(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		ParseFileController(w, r, "guest/index","")
+		return
+	} else if r.URL.Path == "/home" {
+		ParseFileController(w, r, "user/index" , "")
+		
+	} else {
+		ErrorController(w, r, http.StatusNotFound)
+		return
+	}
 }
---
+++ app/models/comment.go
@@ -0,0 +1,14 @@
+package models
+
+import "time"
+
+type Comment struct {
+	ID        int       `json:"id"`
+	PostID    int       `json:"post_id"`
+	UserID    int       `json:"user_id"`
+	Content   string    `json:"content"`
+	CreatedAt time.Time `json:"created_at"`
+	Username  string    `json:"username"`
+	Likes     int       `json:"likes"`
+	Dislikes  int       `json:"dislikes"`
+}
