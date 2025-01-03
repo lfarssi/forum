3bb664e37392827c24c25b651c4d829eac3ea4e6
lfarssi <medlfarssi10@gmail.com>
2025-01-03 17:22:42 +0100

    some models

---
+++ app/http/controllers/parseFileController.go
@@ -0,0 +1,24 @@
+package controllers
+
+import (
+	"fmt"
+	"net/http"
+	"text/template"
+)
+
+func ParseFileController(w http.ResponseWriter, r *http.Request, filename string, data any) {
+	filepath := "./resources/views/" + filename + ".html"
+	// fmt.Println("filepath: ", filepath)
+	temp, err := template.ParseFiles(filepath)
+	if err != nil {
+		fmt.Println("error parsing; ", err)
+		ErrorController(w, r, http.StatusInternalServerError)
+		return
+	}
+	err1 := temp.Execute(w, data)
+	if err1 != nil {
+		fmt.Println("error executing; ", err1)
+		ErrorController(w, r, http.StatusInternalServerError)
+		return
+	}
+}
---
+++ app/models/post.go
@@ -0,0 +1,15 @@
+package models
+
+import "time"
+
+type Posts struct {
+	ID         int              `json:"id"`
+	Title      string           `json:"title"`
+	Content    string           `json:"content"`
+	Categories []string         `json:"categories"`
+	Likes      int              `json:"likes"`
+	Dislikes   int              `json:"dislikes"`
+	CreatedAt  time.Time        `json:"created_at"`
+	Comments   []Comment `json:"comments"`
+	Username   string           `json:"username"`
+}
