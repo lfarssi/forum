fe6ef37059c6ad73bda0eb32d4b6296fa2247b2f
lfarssi <medlfarssi10@gmail.com>
2025-01-21 21:14:17 +0100

    post creation

--- app/http/controllers/postController.go
+++ app/http/controllers/postController.go
@@ -3,5 +3,12 @@ package controllers
 import "net/http"
 
 func PostController(w http.ResponseWriter, r *http.Request) {
-
+	if r.Method != "GET" {
+		ErrorController(w, r, http.StatusMethodNotAllowed, "")
+		return
+	}
+	if r.URL.Path != "/posts" {
+		ErrorController(w, r, http.StatusNotFound, "")
+	}
+	ParseFileController(w, r, "users/create_poste", nil)
 }
