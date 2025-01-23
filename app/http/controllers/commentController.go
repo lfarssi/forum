854870fcc301fa613f03d9214996c21ef94b14c0
lfarssi <medlfarssi10@gmail.com>
2025-01-23 16:24:31 +0100

    creating module and controller for the commment creation

--- app/http/controllers/commentController.go
+++ app/http/controllers/commentController.go
@@ -1,7 +1,25 @@
 package controllers
 
-import "net/http"
+import (
+	"net/http"
+	"time"
+
+	"forum/app/models"
+)
 
 func CommentController(w http.ResponseWriter, r *http.Request) {
+}
 
+func CreatCommentController(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		ErrorController(w, r, http.StatusMethodNotAllowed, "")
+		return
+	}
+	r.ParseForm()
+	var comment models.Comment
+	comment.Content = r.FormValue("content")
+	comment.CreatedAt= time.Now()
+	comment.PostID= r.FormValue("post_id")
+	comment.UserID= 1 // func get user after
+	error := models.CreatComment(comment)
 }
