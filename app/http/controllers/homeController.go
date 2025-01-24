2953a7763822b42ada5aeb28649c4d74479ea79a
hvtims <timo.goog03@gmail.com>
2025-01-24 12:24:37 +0100

    done

--- app/http/controllers/homeController.go
+++ app/http/controllers/homeController.go
@@ -1,20 +1,31 @@
 package controllers
 
 import (
+	"fmt"
+	"net/http"
+
 	"forum/app/models"
 	"forum/utils"
-	"net/http"
 )
 
 func HomeController(w http.ResponseWriter, r *http.Request) {
 	var logedIn bool
+	comments, err := models.GetComments(1)
+	if err != nil {
+		fmt.Println("zaba")
+		ErrorController(w, r, http.StatusInternalServerError, "")
+		return
+	}
+
 	if !utils.IsLoggedIn(r) {
 		logedIn = false
 	} else {
 		logedIn = true
 	}
+	fmt.Println(comments)
 	data := models.Data{
 		IsLoggedIn: logedIn,
+		Comment:    comments,
 	}
 	if r.Method == "GET" {
 		if r.URL.Path != "/" {
