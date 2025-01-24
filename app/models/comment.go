479ee470770b2e1cfc64a2497129645c4dc86ac4
lfarssi <medlfarssi10@gmail.com>
2025-01-24 12:24:37 +0100

    done

--- app/models/comment.go
+++ app/models/comment.go
@@ -1,6 +1,9 @@
 package models
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 type Comment struct {
 	ID        int       `json:"id"`
@@ -25,4 +28,36 @@ func CreatComment(comment Comment) error {
 		return err
 	}
 	return nil
+}
+
+func GetComments(postid int) ([]Comment, error) {
+	qwiri := `
+	SELECT c.id, c.content, c.user_id, c.date_creation,  c.post_id , u.username
+	FROM comments c
+	INNER JOIN  users u 
+	ON u.id = c.user_id 
+	WHERE post_id = ?
+	 ORDER BY date_creation DESC;	
+	`
+	rows, err := Database.Query(qwiri, postid)
+	if err != nil {
+		fmt.Println("awal error")
+		return nil, err
+	}
+	defer rows.Close()
+	var comments []Comment
+	for rows.Next() {
+
+		var c Comment
+		if err := rows.Scan(&c.ID,&c.Content, &c.UserID,  &c.CreatedAt, &c.PostID, &c.Username); err != nil {
+			fmt.Println("error 2")
+			return nil, err
+		}
+		comments = append(comments, c)
+	}
+	if err := rows.Err(); err != nil {
+		fmt.Println("eroor 3")
+		return nil, err
+	}
+	return comments, nil
 }
