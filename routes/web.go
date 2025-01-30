cc884be1c369f4005413376a9e43a4f483132fa5
lfarssi <medlfarssi10@gmail.com>
2025-01-30 17:54:44 +0100

    fart

--- routes/web.go
+++ routes/web.go
@@ -9,7 +9,7 @@ import (
 func WebRouter() {
 	http.HandleFunc("/", controllers.HomeController)
 	http.HandleFunc("/PostByCategories", controllers.PostByCategoriesController)
-	http.HandleFunc("/createdPost", controllers.CreatedPostController)
+	http.HandleFunc("/createdPost",middleware.AuthMiddleware(  controllers.CreatedPostController))
 	http.HandleFunc("/myliked",middleware.AuthMiddleware( controllers.LikedPostController))
 	http.HandleFunc("/login", middleware.AlreadyLoggedIn(controllers.ParseLogin))
 	http.HandleFunc("/register", middleware.AlreadyLoggedIn(controllers.ParseRegister))
