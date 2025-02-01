c60392db9be3fddfe6a1ecf76d2d6d0c94a68c6e
fahdaguenouz <faguenouz@gmail.com>
2025-02-01 13:57:56 +0100

    adding the ssl and the password for database

--- Readme.md
+++ Readme.md
@@ -135,4 +135,5 @@ FORUM
 
 
 
-
+##
+openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365 -nodes
--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
+	"log"
 
 	"forum/app/models"
 	"forum/routes"
 	"net/http"
+
 )
 
 
@@ -14,10 +17,20 @@ func main() {
 	defer models.CloseDatabase()
 	routes.WebRouter()
 	routes.ApiRouter()
-	fmt.Println("Server is running on http://localhost:8080")
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		fmt.Println("err starting the server : ", err)
-		return
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: nil,
+		TLSConfig: &tls.Config{
+			MinVersion:               tls.VersionTLS12, // Enforce TLS 1.2+
+			PreferServerCipherSuites: true,
+			CurvePreferences:         []tls.CurveID{tls.CurveP256, tls.X25519},
+		},
 	}
+
+	log.Println("Starting HTTPS server on port 8080...")
+	log.Fatal(server.ListenAndServeTLS("cert.pem", "key.pem"))
+
+
+	fmt.Println("Server is running on https://localhost:8443")
+	
 }
