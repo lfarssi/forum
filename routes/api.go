6732b03cae7205869fad0841c7fe8c2c31cdad2b
lfarssi <medlfarssi10@gmail.com>
2025-02-01 15:22:00 +0100

    finaly

--- routes/api.go
+++ routes/api.go
@@ -11,6 +11,7 @@ func ApiRouter() {
 	http.HandleFunc("/signIn", controllers.LoginController)
 	http.HandleFunc("/signUp", controllers.RegisterController)
 	http.HandleFunc("/logout", controllers.LogoutController)
+	http.HandleFunc("/delete_post", controllers.DeleteController)
 	http.HandleFunc("/create_post", middleware.AuthMiddleware(middleware.RateLimitMiddleware( controllers.CreatePosts)))
 	http.HandleFunc("/react",middleware.AuthMiddleware(middleware.RateLimitMiddleware( controllers.ReactPostController)))
 	http.HandleFunc("/create_comment",middleware.AuthMiddleware(middleware.RateLimitMiddleware( controllers.CreateCommentController)))
