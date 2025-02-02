011ed7b4cbe454aff36ac530668fa3855d9e4ffd
lfarssi <2002060600366@ofppt-edu.ma>
2025-02-02 13:12:21 +0100

    finaly

--- routes/web.go
+++ routes/web.go
@@ -11,6 +11,6 @@ func WebRouter() {
 	http.HandleFunc("/PostByCategories", controllers.PostByCategoriesController)
 	http.HandleFunc("/createdPost",middleware.AuthMiddleware(  controllers.CreatedPostController))
 	http.HandleFunc("/myliked",middleware.AuthMiddleware( controllers.LikedPostController))
-	http.HandleFunc("/login", middleware.AlreadyLoggedIn(middleware.RateLimitMiddleware(controllers.ParseLogin)))
-	http.HandleFunc("/register", middleware.AlreadyLoggedIn(middleware.RateLimitMiddleware(controllers.ParseRegister)))
+	http.HandleFunc("/login", middleware.AlreadyLoggedIn(controllers.ParseLogin))
+	http.HandleFunc("/register", middleware.AlreadyLoggedIn(controllers.ParseRegister))
 }
