1b37a3cd60cfbdd8177a0d5347e1984727b7a4c9
lfarssi <medlfarssi10@gmail.com>
2025-02-08 15:16:34 +0100

    backend auth done

--- routes/api.go
+++ routes/api.go
@@ -9,13 +9,14 @@ import (
 func ApiRouter() {
 
 	http.HandleFunc("/signIn", controllers.LoginController)
-	http.HandleFunc("/signUp",controllers.RegisterController)
+	http.HandleFunc("/signUp", controllers.RegisterController)
 	http.HandleFunc("/logout", controllers.LogoutController)
+	http.HandleFunc("/redirect", middleware.AlreadyLoggedIn(controllers.HandleGoogleLogin))
+	http.HandleFunc("/callback", controllers.HandleGoogleCallback)
 	// http.HandleFunc("/delete_post", controllers.DeleteController)
-	http.HandleFunc("/create_post", middleware.AuthMiddleware(middleware.RateLimitMiddleware( controllers.CreatePosts)))
-	http.HandleFunc("/react",middleware.AuthMiddleware(middleware.RateLimitMiddleware( controllers.ReactPostController)))
-	http.HandleFunc("/create_comment",middleware.AuthMiddleware(middleware.RateLimitMiddleware( controllers.CreateCommentController)))
+	http.HandleFunc("/create_post", middleware.AuthMiddleware(middleware.RateLimitMiddleware(controllers.CreatePosts)))
+	http.HandleFunc("/react", middleware.AuthMiddleware(middleware.RateLimitMiddleware(controllers.ReactPostController)))
+	http.HandleFunc("/create_comment", middleware.AuthMiddleware(middleware.RateLimitMiddleware(controllers.CreateCommentController)))
 	http.HandleFunc("/resources/", controllers.CssJsController)
-	
 
 }
